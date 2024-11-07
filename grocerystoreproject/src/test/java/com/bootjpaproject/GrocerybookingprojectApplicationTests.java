c529c8cd8ef35f470138afcb5203502e18e66029
amolsuryabhans <amolsuryabhans@apporchid.com>
2024-11-07 12:05:48 +0530

    qp-assessment for grocery booking api

---
+++ grocerystoreproject/src/test/java/com/bootjpaproject/GrocerybookingprojectApplicationTests.java
@@ -0,0 +1,15 @@
+package com.bootjpaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.grocerystore.GroceryStoreProjectApplication;
+
+@SpringBootTest(classes = GroceryStoreProjectApplication.class)
+class GrocerybookingprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
